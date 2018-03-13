f4abad8b688304666e79d4fd94434b7777774002
Diego Gilon <diego.gilon@gmail.com>
2018-03-13 15:25:52 +0100

    in progress

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'tasks/new', to: 'tasks#new', as: :new
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch 'tasks/:id', to: 'tasks#update'
+  patch 'task/:id', to: 'tasks#update'
+  put 'task/:id', to: 'tasks#update'
   delete 'task/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
